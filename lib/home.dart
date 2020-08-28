0adf604d7f59c54e3d754c01eb371bc3ee9ddec8
Esmaeil Ahmadipour <esmaeil-ahmadipour@users.noreply.github.com>
2020-08-28 08:29:53 +0430

    Add Download Method

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -6,8 +9,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool downloading = false;
+
+  String progress = '0';
+
+  bool isDownloaded = false;
+
+  String uri =
+      'https://ea2.ir/uploads/a977.png'; // url of the file to be downloaded
+
+  String filename = 'a977.png'; // file name that you desire to keep
+
+  // downloading logic is handled by this method
+
+  Future<void> downloadFile(uri, fileName) async {
+    setState(() {
+      downloading = true;
+    });
+
+    String savePath = await getFilePath(fileName);
+
+    Dio dio = Dio();
+
+    dio.download(
+      uri,
+      savePath,
+      onReceiveProgress: (rcv, total) {
+        print(
+            'received: ${rcv.toStringAsFixed(0)} out of total: ${total.toStringAsFixed(0)}');
+
+        setState(() {
+          progress = ((rcv / total) * 100).toStringAsFixed(0);
+        });
+
+        if (progress == '100') {
+          setState(() {
+            isDownloaded = true;
+          });
+        } else if (double.parse(progress) < 100) {}
+      },
+      deleteOnError: true,
+    ).then((_) {
+      setState(() {
+        if (progress == '100') {
+          isDownloaded = true;
+        }
+
+        downloading = false;
+      });
+    });
+  }
+
+  //gets the applicationDirectory and path for the to-be downloaded file
+
+  // which will be used to save the file to that path in the downloadFile method
+
+  Future<String> getFilePath(uniqueFileName) async {
+    String path = '';
+
+    Directory dir = await getApplicationDocumentsDirectory();
+
+    path = '${dir.path}/$uniqueFileName';
+
+    return path;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    print('build running');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Download By DIO'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('$progress%'),
+              isDownloaded
+                  ? Text(
+                'File Downloaded! You can see your file in the application\'s directory',
+              )
+                  : Text(
+                  'Click the FloatingActionButton to start Downloading!'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () async {
+            downloadFile(uri, filename);
+          }),
+    );
   }
 }
