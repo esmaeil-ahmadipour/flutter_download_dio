1f0a5667002ff886ffdfdffdac93801bd588c410
Esmaeil Ahmadipour <esmaeil-ahmadipour@users.noreply.github.com>
2020-08-28 07:58:37 +0430

    Initial commit

---
+++ android/app/src/main/kotlin/ir/ea2/flutter_download_dio/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.ea2.flutter_download_dio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_download_dio/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
